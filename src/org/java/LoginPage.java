3c888da8281a49115737fd089ea0ea2f0f202cb2
Kalaivani23 <vanikalai232@gmail.com>
2019-05-19 19:17:31 +0530

    Mod code added

--- src/org/java/LoginPage.java
+++ src/org/java/LoginPage.java
@@ -12,10 +12,16 @@ public class LoginPage {
 	public void multi() {
 		System.out.println("Multi");
 	}
+	
+	public void mod() {
+		System.out.println("Mod");
+		System.out.println("Kalaivani");
+	}
 	public static void main(String[] args) {
 		LoginPage l = new LoginPage();
 		l.add();
 		l.sub();
 		l.add();
+		l.mod();
 	}
 }
